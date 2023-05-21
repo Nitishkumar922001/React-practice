69e6560f6f557b02e9de5239ec5dbc462be8dea7
Nitishkumar922001 <miranamnitish@gmail.com>
2023-05-21 08:54:21 +0530

    createPortal concept

--- users/src/components/UI/ErrorDialogBox.js
+++ users/src/components/UI/ErrorDialogBox.js
@@ -1,16 +1,29 @@
-import './ErrorPopUp.css'
+import './ErrorPopUp.css';
+import ReactDom from 'react-dom';
+function BlackDrop(props)
+{
+  return <div className="blackDrop"  onClick={props.errHandler}></div>
+}
+function ErrorModal(props){
+  return <div className="err-popup">
+  <header>
+    <h2>invalid input</h2>
+  </header>
+  <p>{props.errormsg}</p>
+  <footer>
+    <button onClick={props.errHandler}>Okey</button>
+  </footer>
+</div>
+}
+
 function ErrorDialogBox(props) {
- return <div  >
-    <div className="blackDrop"  onClick={props.errHandler}></div>
-    <div className="err-popup">
-      <header>
-        <h2>invalid input</h2>
-      </header>
-      <p>{props.errormsg}</p>
-      <footer>
-        <button onClick={props.errHandler}>Okey</button>
-      </footer>
-    </div>
-  </div>;
+ return <>
+   {
+    ReactDom.createPortal(<BlackDrop errHandler={props.errHandler}/>,document.getElementById('overlay'))
+   }
+   {
+    ReactDom.createPortal(<ErrorModal errormsg={props.errormsg} errHandler={props.errHandler}/>,document.getElementById('error-modal'))
+   }
+  </>;
 }
 export default ErrorDialogBox;

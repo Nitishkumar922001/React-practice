2b20e67d95ddf2f45b8594aafb2e1afec7a72e67
Nitishkumar922001 <miranamnitish@gmail.com>
2023-05-28 08:27:28 +0530

    get request done

--- HowSendHttpReq/src/App.js
+++ HowSendHttpReq/src/App.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState();
+  const [Loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
+  
+  async function movieFetchHandler() {
+    try {
+      setLoading(true);
+      const respone = await fetch("https://swapi.dev/api/films");
+      if (!respone.ok) {
+        throw new Error("something went wrong");
+      }
+      const data = await respone.json();
+      console.log(data);
+      setMovies(data);
+    } catch (Error) {
+      console.log(Error);
+      setLoading(false);
+      setError(Error.message);
+    }
+    setLoading(false);
+  }
+  useEffect(()=>{movieFetchHandler()},[])
+  let content;
+  if (Loading) {
+    content = <p>...Loading</p>;
+  } else if (error) {
+    content = <h4>{error}</h4>;
+  } else if (movies) {
+    content = <MoviesList movies={movies} />;
+  } else {
+    content = <p>no movies</p>;
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={movieFetchHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- HowSendHttpReq/src/components/MoviesList.js
+++ HowSendHttpReq/src/components/MoviesList.js
@@ -6,12 +6,12 @@ import classes from './MoviesList.module.css';
 const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
+      {props.movies.results.map((movie) => (
         <Movie
-          key={movie.id}
+          key={movie.episode_id}
           title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
+          releaseDate={movie.release_date}
+          openingText={movie.opening_crawl}
         />
       ))}
     </ul>
